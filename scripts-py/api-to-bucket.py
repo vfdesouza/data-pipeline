1a4dbcb479ed812a3c4aec155e25ea32927681a5
vfdesouza <vfdesouza@outlook.com>
2023-01-23 22:29:15 -0300

    created the logic to access the api, capture the file, save it on the local machine, send it to the bucket and remove the file from the local machine

--- scripts-py/api-to-bucket.py
+++ scripts-py/api-to-bucket.py
@@ -1,16 +1,37 @@
 from module import Module
+from time import sleep
+import requests as r
+import os
 
 obj = Module()
 
-bucket_name = ''
+bucket_name = 'bucket-quickstart_my-project-datapipeline-375319'
 bucket = obj.initialize_bucket(bucket_name=bucket_name)
 
-file_to_upload = ''
-path_to_file = ''
+color = ['yellow', 'green', 'fhv', 'fhvhv']
+month = ['07', '08', '09', '10', '11', '12']
 
-obj.upload_to_bucket(
-  blob_name=file_to_upload,\
-  path_to_file=path_to_file,\
-  bucket=bucket)
+base_url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/%s_tripdata_2022-%s.parquet'
+
+for c in color:
+    for m in month:
+
+        url = str(base_url % (c, m))
+        filename = f'{c}_tripdata_2022_{m}.parquet'
+        response = r.get(url, allow_redirects=True)
+
+        with open('./temparq/' + filename, 'wb') as file:
+            file.write(response.content)
+            file.close()
+
+        sleep(3)
+
+        obj.upload_to_bucket(
+            blob_name=filename,
+            path_to_file=f'./temparq/{filename}',
+            bucket=bucket)
+
+        path = os.path.join('./temparq/', filename)
+        os.remove(path)
 
 print('Process concluded!')
--- scripts-py/bucket-to-bq.py
+++ scripts-py/bucket-to-bq.py
@@ -2,14 +2,14 @@ from module import Module
 
 obj = Module()
 
-bucket_name=''
-blob_name=''
-path_folder=''
+bucket_name = ''
+blob_name = ''
+path_folder = ''
 
 bucket = obj.initialize_bucket(bucket_name=bucket_name)
 
-obj.download_to_bucket(bucket=bucket,\
-                      blob_name=blob_name,\
-                      path_folder=path_folder)
+obj.download_to_bucket(bucket=bucket,
+                       blob_name=blob_name,
+                       path_folder=path_folder)
 
 print('Process concluded!')
